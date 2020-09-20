01b1bac3d396ac1bd3a11245efa09c383cc200ee
Elbie-em <emoonga1993@gmail.com>
2020-09-20 17:31:03 +0200

    Added children props

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Greet from './components/Greet'
 const App = () => {
   return (
     <div className="App">
-      <Greet name="JavaScript" level="Root"/>
-      <Greet name="React" level="Node"/>
+      <Greet name="JavaScript" level="Root">
+        <h3>I am a child H3 tag</h3>
+      </Greet>
+      <Greet name="React" level="Node">
+        <button onClick={() => alert("I am a Child")}>I am a Child Button, Click Me</button>
+      </Greet>
       <Greet name="Redux" level="Node"/>
     </div>
   );
