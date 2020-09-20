a77a8bc0472505d2e88dee239be86b2780620188
Elbie-em <emoonga1993@gmail.com>
2020-09-20 17:27:17 +0200

    Add props.children

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Greet = (props) => {
   return (
-    <h1>Hello, I am {props.name} and my Level is {props.level}</h1>
+    <div>
+      <h1>Hello, I am {props.name} and my Level is {props.level}</h1>
+      {props.children}
+    </div>
   )
 }
 
