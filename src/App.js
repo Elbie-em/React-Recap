bb16033ed3a5a3d920b3dcc567f4bf1792902867
Elbie-em <emoonga1993@gmail.com>
2020-09-21 00:29:29 +0200

    Render Message in App component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
-import Welcome from './components/Welcome'
+import Message from './components/Message'
 
 const App = () => {
   return (
     <div className="App">
-      <Greet name="JavaScript" level="Root">
-        <h3>I am a child H3 tag</h3>
-      </Greet>
-      <Greet name="React" level="Node">
-        <button onClick={() => alert("I am a Child")}>I am a Child Button, Click Me</button>
-      </Greet>
-      <Greet name="Redux" level="Node"/>
-
-      <Welcome name="Coding" level="Parent"/>
+      <Message />
     </div>
   );
 }
