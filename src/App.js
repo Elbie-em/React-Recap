f9e28db1ec12dd761049be167a7d391727b7f888
Elbie-em <emoonga1993@gmail.com>
2020-09-27 20:51:18 +0200

    Render counter component in App Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Message from './components/Message'
+import Counter from './components/Counter'
+
 
 const App = () => {
   return (
     <div className="App">
       <Message />
+      <Counter />
     </div>
   );
 }
