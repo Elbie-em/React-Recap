1d43a1b829efcc39ac1b7e43a81958ddf80c62e6
Elbie-em <emoonga1993@gmail.com>
2020-09-28 10:30:02 +0200

    Add increment handler, pass increment to button onClick

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,11 +8,18 @@ class Counter extends Component {
     }
   }
 
+  increment () {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
   render() {
     return (
       <div>
       <br />
       <h2>Count: {this.state.count}</h2>
+      <button onClick={() => this.increment()}>Increment</button>
       </div>
     )
   }
