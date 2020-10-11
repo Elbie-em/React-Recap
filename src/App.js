1688c887ba12455fd93c231ec242b4b067f565c0
Elbie-em <emoonga1993@gmail.com>
2020-10-11 14:34:44 +0200

    Import and render ClassClick

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionClick from './components/FunctionClick'
-
+import ClassClick from './components/ClassClick'
 
 
 const App = () => {
   return (
     <div className="App">
       <FunctionClick />
+      <br />
+      <br />
+      <ClassClick />
     </div>
   );
 }
