8fdee290f370d79362b3a22719c487d2b5bd06ad
Elbie-em <emoonga1993@gmail.com>
2020-10-12 14:36:21 +0200

    Add button, Add state with message property

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 class EventBind extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       message: "Hello World, Nice to meet you"
+    }
+  }
+  
   render() {
     return (
       <div>
-        
+        <h2>{this.state.message}</h2>
+        <br />
+        <br />
+        <button>Bind Click Me</button>
       </div>
     )
   }
