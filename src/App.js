dd275c1084716668d40b270fa5a8c6995f35594d
Elbie-em <emoonga1993@gmail.com>
2020-10-12 14:33:25 +0200

    Import and render EventBind

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
 
 
 const App = () => {
   return (
     <div className="App">
+      <EventBind />
+      <br />
+      <br />
       <FunctionClick />
       <br />
       <br />
