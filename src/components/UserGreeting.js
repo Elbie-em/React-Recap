34adb12f59e85d3458b70cba156d0e5f5e1b9692
Elbie-em <emoonga1993@gmail.com>
2020-10-26 18:49:42 +0200

    Add constructor and state

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 class UserGreeting extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       isLoggedIn : false,
+    }
+  }
+  
   render() {
     return (
       <div>
-        <h1>Welcome to react</h1>
+        <h1>Welcome to Guest</h1>
+        <h1>Welcome to React</h1>
       </div>
     )
   }
