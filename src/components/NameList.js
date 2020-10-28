9d693bdff97907f22113bc249ac91af745a1dfda
Elbie-em <emoonga1993@gmail.com>
2020-10-28 14:28:28 +0200

    Add List rendering with map method

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const NameList = () => {
+  const langs = ["Java", "C#", "Ruby","Python"]
+
   return (
     <div>
-      
+      {
+        langs.map((lang) => <h2>{lang}</h2> )
+      }
     </div>
   )
 }
