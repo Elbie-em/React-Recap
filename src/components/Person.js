3f2d7a63b8ba6d6b7e9bb88ddea43b14fefb4405
Elbie-em <emoonga1993@gmail.com>
2020-10-28 14:37:46 +0200

    Add Person component

---
+++ src/components/Person.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Person = () => {
+  return (
+    <div>
+      <h2> I am {person.name} and I program in {person.skill} </h2>
+    </div>
+  )
+}
+
+export default Person
