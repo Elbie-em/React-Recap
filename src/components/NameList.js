478727e8096034d17b9e8eca7674d2875d77f793
Elbie-em <emoonga1993@gmail.com>
2020-10-29 14:48:24 +0200

    Iterate through persons and render result to NameList component

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Person from './Person'
 
 const NameList = () => {
   const langs = ["Java", "C#", "Ruby","Python"]
@@ -22,11 +23,16 @@ const NameList = () => {
     }
   ]
 
+  const personList = persons.map((person) => <Person person={person}/>)
+
   return (
     <div>
       {
         langs.map((lang) => <h2>{lang}</h2> )
       }
+    <div>
+      {personList}
+    </div>
     </div>
   )
 }
