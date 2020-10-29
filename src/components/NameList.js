d1914be4151831e74e7a4608e0be63a97ecb6d1c
Elbie-em <emoonga1993@gmail.com>
2020-10-29 14:43:31 +0200

    Add persons array with person objects

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -3,6 +3,25 @@ import React from 'react'
 const NameList = () => {
   const langs = ["Java", "C#", "Ruby","Python"]
 
+  const persons = [
+    {
+      name: "Jack",
+      skill: langs[0]
+    },
+    {
+      name: "Jill",
+      skill: langs[2]
+    },
+    {
+      name: "Jim",
+      skill: langs[3]
+    },
+    {
+      name: "Jade",
+      skill: langs[1]
+    }
+  ]
+
   return (
     <div>
       {
