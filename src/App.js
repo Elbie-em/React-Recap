e7752b3fdbb597d00445f97a5d052a78e0ebe00f
Elbie-em <emoonga1993@gmail.com>
2020-11-08 18:02:15 +0200

    Import HoverCounter component and render in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import UserGreeting from './components/UserGreeting'
 import ClickCounter from './components/ClickCounter'
+import HoverCounter from './components/HoverCount';
 
 const App = () => {
   return (
     <div className="App">
       <br />
+      <HoverCounter />
       <br />
       <ClickCounter />
       <br />
