7f58fbb467ab03db4dd4b683379a2647e81e737a
Elbie-em <emoonga1993@gmail.com>
2020-11-11 02:29:22 +0200

    Move component shared functionality ClickCounter and HoverCounter

--- src/components/withCounter.js
+++ src/components/withCounter.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const UpdatedComponent = (OriginalComp) => {
-  class NewComponent extends Component {
+  class NewComponent extends React.Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count: 0
+      }
+    }
+  
+    incrementCount = () => {
+      this.setState(prevState => {
+        return {count: prevState.count + 1}
+      })
+    }
+
     render () {
-      return <OriginalComp name='HOC'/>
+      return <OriginalComp count={this.state.count} incrementCount={this.incrementCount}/>
     }
   }
 
