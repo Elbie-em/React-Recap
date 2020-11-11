eb3ea495b2f2ff4afa75be9cac421ce15036b6cb
Elbie-em <emoonga1993@gmail.com>
2020-11-11 02:30:11 +0200

    Destructure props from higher order component and pass to respective elements

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react'
-class ClickCounter extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       count: 0
-    }
-  }
+import UpdatedComponent from './withCounter'
 
-  incrementCount = () => {
-    this.setState(prevState => {
-      return {count: prevState.count + 1}
-    })
-  }
-  
+class ClickCounter extends Component {
   render() {
-    const {count} = this.state
+    const {count, incrementCount} = this.props
     return (
       <div>
-        <button onClick={this.incrementCount}>Clicked {count} times</button>
+        <button onClick={incrementCount}>Clicked {count} times</button>
       </div>
     )
   }
 }
 
-export default ClickCounter
+export default UpdatedComponent(ClickCounter)
