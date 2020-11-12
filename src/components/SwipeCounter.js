77900f75be2d1dd0beb4b7562ca351fde0c547a0
Elbie-em <emoonga1993@gmail.com>
2020-11-12 15:12:26 +0200

    Add state and pass incrementCount to mouseOver button attribute

--- src/components/SwipeCounter.js
+++ src/components/SwipeCounter.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
-export class SwipeCounter extends Component {
+class SwipeCounter extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       count: 0
+    }
+  }
+
+  incrementCount = () => {
+    this.setState(prevState => {
+      return {count : prevState.count + 1}
+    })
+  }
+  
   render() {
+    const {count} = this.state
     return (
       <div>
-        
+        <button onMouseOver={this.incrementCount}>Swiped {count} times</button>
       </div>
     )
   }
