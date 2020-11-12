0ca585e55262cb5327dd285915549e4a5fdbab7b
Elbie-em <emoonga1993@gmail.com>
2020-11-12 15:08:45 +0200

    Add SwipeCounter component

---
+++ src/components/SwipeCounter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SwipeCounter extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default SwipeCounter
