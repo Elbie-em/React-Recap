a71af84a79a821be9dab08125ffa12e996563f0b
Elbie-em <emoonga1993@gmail.com>
2020-11-12 14:59:38 +0200

    Add Presscounter component

---
+++ src/components/PressCounter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PressCounter extends Component {
+  render() {
+    return (
+      <div>
+        <button>Pressed X times</button>
+      </div>
+    )
+  }
+}
+
+export default PressCounter
