b8e13a2ae0ef51adbc6802ca7f1412d021fd4b85
Elbie-em <emoonga1993@gmail.com>
2020-11-14 18:22:11 +0200

    Move redundant code  to CountPS

--- src/components/SwipeCounter.js
+++ src/components/SwipeCounter.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react'
 
 class SwipeCounter extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       count: 0
-    }
-  }
-
-  incrementCount = () => {
-    this.setState(prevState => {
-      return {count : prevState.count + 1}
-    })
-  }
-  
   render() {
-    const {count} = this.state
+    const {count,incrementCount} = this.props
     return (
       <div>
-        <button onMouseOver={this.incrementCount}>Swiped {count} times</button>
+        <button onMouseOver={incrementCount}>Swiped {count} times</button>
       </div>
     )
   }
