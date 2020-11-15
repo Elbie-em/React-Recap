7a4ac142044023d7064001748344c19732489d2e
Elbie-em <emoonga1993@gmail.com>
2020-11-15 16:56:25 +0200

    Render ComponentF

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import ComponentF from './ComponentF';
 
 class ComponentE extends Component {
   render() {
     return (
       <div>
-        
+        <ComponentF />
       </div>
     )
   }
