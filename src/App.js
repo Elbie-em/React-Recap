6b03dee8d63d491d5aef7d8ff5eb92e576041213
Elbie-em <emoonga1993@gmail.com>
2020-11-15 16:40:35 +0200

    Remove unescessary imports; import ComponentC and render in App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import './App.css';
-import CountPS from './components/CountPS';
-import PressCounter from './components/PressCounter';
-import SwipeCounter from './components/SwipeCounter';
-import User from './components/User'
+import ComponentC from './components/ComponentC';
+
 
 const App = () => {
   return (
     <div className="App">
-      <CountPS render={(count, incrementCount) => 
-        <PressCounter count={count} incrementCount={incrementCount}/>
-      }/>
-      <CountPS render={(count, incrementCount) => 
-        <SwipeCounter count={count} incrementCount={incrementCount}/>
-      }/>
-      {/* <br />
-      <PressCounter />
-      <br />
-      <SwipeCounter />
-      <br />
-      <User render={(isLoggedIn) => isLoggedIn ? 'React' : 'Guest'}/> */}
+      <ComponentC />
     </div>
   );
 }
