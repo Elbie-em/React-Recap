9b2d30ec4a39e1584b76b342880e41855b813863
Elbie-em <emoonga1993@gmail.com>
2020-11-16 17:02:47 +0200

    Wrap componentC in UserProvider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import ComponentC from './components/ComponentC';
+import {UserProvider} from './components/userContext';
 
 
 const App = () => {
   return (
     <div className="App">
-      <ComponentC />
+      <UserProvider value="Kranium">
+        <ComponentC />
+      </UserProvider>
     </div>
   );
 }
