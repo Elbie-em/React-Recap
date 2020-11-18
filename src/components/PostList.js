8a9c3f64f4c3e2ff2116747184a4620fafe12e2d
Elbie-em <emoonga1993@gmail.com>
2020-11-18 14:02:46 +0200

    Import Axios and add constructor

---
+++ src/components/PostList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class PostList extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <h3>Lists of Posts</h3>
+      </div>
+    )
+  }
+}
+
+export default PostList
