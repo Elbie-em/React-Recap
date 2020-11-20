56b6edcb56f3f86c43dc1bfd5ee1b514a7580ab2
Elbie-em <emoonga1993@gmail.com>
2020-11-20 22:10:43 +0200

    Add constructor with state, add changeHandler

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 
 class PostForm extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       userId:'',
+       title:'',
+       body:''
+    }
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  
   render() {
     return (
       <div>
         <form>
           <div>
-            <input type="text" name="userId" />
+            <input type="text" name="userId"/>
           </div>
           <div>
             <input type="text" name="title" />
